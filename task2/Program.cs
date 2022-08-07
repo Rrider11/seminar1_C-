490a8e4a69a09a2b1c97b617e8729af9a7f87a83
Manakov Aleksandr <manakov.shura@yandex.ru>
2022-08-07 22:36:22 +0300

    ввод двух чисел и вывод максимального из них

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введите целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if( a > b ) max = a;
+if(b > a) max = b;
+
+
+Console.WriteLine("Max =" + " " + max);
+
