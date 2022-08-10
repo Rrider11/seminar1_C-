a2df1449ad38df6add733914948bd9fdc7cd52b2
Manakov Aleksandr <manakov.shura@yandex.ru>
2022-08-10 17:05:34 +0300

    дни недели , спать или работать

---
+++ task15/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите число от 1 до 7");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+if((N == 6) ^ (N == 7))
+{
+    Console.WriteLine("Сегодня выходной! спи");
+}
+else
+{
+   Console.WriteLine("Вставай пора на работу! :)"); 
+}
