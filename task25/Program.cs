f4c7a6a259622ad4b5552dc9f71043891765ca81
Manakov Aleksandr <manakov.shura@yandex.ru>
2022-08-19 08:38:18 +0300

    вводим а и б и возводим а в степень б

---
+++ task25/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит
+//  число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число a");
+
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число b");
+
+double b = Convert.ToDouble(Console.ReadLine());
+
+
+while(b > 0)
+{
+    double ab = (Math.Pow(a,b));
+    Console.Write( ab );
+    break;
+
+}
+
+    Console.Write( "число B должно быть > 0" );
+
+
+
+
+
+
