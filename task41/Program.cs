36dc42fc1eea1318369a36e2954f0973f7aa21ad
Manakov Aleksandr <manakov.shura@yandex.ru>
2022-08-26 08:25:30 +0300

    выводим массив и считаем сксолько положительных чисел

---
+++ task41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+
+
+
+int[] mass = new int[5];
+int countPlus = 0;
+
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = int.Parse(Console.ReadLine());
+
+    // Console.Write(string.Join(",", mass));
+
+    
+
+    if (mass[i] > 0)
+    {
+        countPlus++;
+    }
+}
+
+
+Console.Write($"Число положительных чисел в массиве = {countPlus}");
