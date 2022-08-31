392eae4b25af5400e100ef73c553f2b0a646fb90
Manakov Aleksandr <manakov.shura@yandex.ru>
2022-08-31 12:24:11 +0300

    задача на поиск среднего арифметического каждого столба в двумерном массиве

---
+++ task52/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+
+
+double[,] matrix = new double[3, 4];
+
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(10);
+        }
+    }
+}
+
+Console.WriteLine();
+FillArray(matrix);
+
+PrintArray(matrix);
+
+void SumColumn(double[,] matr)
+{
+    double summ = 0;
+    for (int i = 0; i < matr.GetLength(1); i++)
+    {
+        for (int j = 0; j < matr.GetLength(0); j++)
+        {
+            summ += matrix[j, i];
+        }
+        Console.WriteLine($" Среднее арифметическое {i+1} столбца равно {summ/matr.GetLength(0)}");
+    }
+}
+
+SumColumn(matrix);
